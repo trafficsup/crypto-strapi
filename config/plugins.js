99f1a303afbfc7b7761ce731d74bb656176d7a41
davidkalifa <accounting@trafficonnet.com>
2026-05-26 09:40:24 +0000

    Add strapi-5-sitemap-plugin — sitemap generation for Strapi Cloud

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = () => ({});
+'use strict';
+
+module.exports = () => ({
+  'strapi-5-sitemap-plugin': {
+    enabled: true,
+    config: {
+      hostname: 'https://crypt-pseo.vercel.app',
+      excludedTypes: [],
+      defaultChangefreq: 'weekly',
+      defaultPriority: 0.7,
+    },
+  },
+});
